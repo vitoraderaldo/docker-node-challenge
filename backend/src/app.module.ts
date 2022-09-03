1ef44fe6084e6a82b8ab6400f914fc814adc582f
Vitor Aderaldo <vitor.aldero@harmonize.health>
2022-09-02 23:48:45 -0300

    feat: add dockerize on nest container

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: 3306,
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      entities: [],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ mysql/start.sql
@@ -0,0 +1,10 @@
+CREATE TABLE User (
+  id int primary key,
+  firstName varchar(50)
+);
+
+INSERT INTO `backend_database`.`User` (`id`, `firstName`) VALUES ('1', 'Vitor');
+INSERT INTO `backend_database`.`User` (`id`, `firstName`) VALUES ('2', 'Renata');
+INSERT INTO `backend_database`.`User` (`id`, `firstName`) VALUES ('3', 'Bruna');
+INSERT INTO `backend_database`.`User` (`id`, `firstName`) VALUES ('4', 'Joao');
+INSERT INTO `backend_database`.`User` (`id`, `firstName`) VALUES ('5', 'Vinicius');
